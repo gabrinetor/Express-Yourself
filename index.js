6117bf8df0a3e2f2ac2bfc0a4b3faab4b6d329af
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-24 19:43:04 -0300

    reading requests datas

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ app.use((req, res, next) => {
     next()
 })
 
+app.get('/clientes/relatorio', (req, res) => { //função mais específica, também poderia ser post, mas sabemos que os parâmetros viessem no body da requisição
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    let corpo = ''
+    req.on('data', function(parte){
+        corpo += parte
+    })
+
+    req.on('end', function() {
+        //res.json(JSON.parse(corpo)) //body parse interpreta e entrega o conteudo a mais resumido pronto no formato de objeto
+        res.send(corpo)
+    })
+})
+
+app.get('/clientes/:id', (req, res) => { //função mais generica
+    res.send(`Cliente ${req.params.id} selecionadx!`)
+})
+
+
 app.get('/ola', (req, res, next) => {
     console.log('Durante...')
     res.json({
